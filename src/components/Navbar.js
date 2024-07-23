f6f460574579a1f75b6c87f587ad961cbfdb23ae
Saurav Singh <souravsingh.ss508@gmail.com>
2024-07-23 23:41:05 +0530

    new navbar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,44 @@
-// components/Navbar.js
-
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logoContainer}>
-        <img src="/images/launchwise-logo.png" alt="Launch-Wise Logo" className={styles.logo} />
+      <div className={styles.logo}>
+        <img src="/images/launchwise-logo.png" className={styles.logo} alt="Launch-Wise Logo" />
       </div>
-      <ul className={styles.navLinks}>
-        <li className={styles.navItem}><a href="#">Home</a></li>
-        <li className={styles.navItem}><a href="#">Product</a></li>
-        <li className={styles.navItem}><a href="#">Pricing</a></li>
-        <li className={styles.navItem}><a href="#">Contact</a></li>
-      </ul>
-      <div className={styles.enquireButtonContainer}>
-        <button className={styles.enquireButton}>Enquire now →</button>
+      <div ref={dropdownRef} className={`${styles.links} ${isOpen ? styles.open : ''}`}>
+        <a href='#' onClick={toggleDropdown}>Home   </a>
+        <a href='#' onClick={toggleDropdown}>Product</a>
+        <a href='#' onClick={toggleDropdown}>Pricing</a>
+        <a href='#' onClick={toggleDropdown}>Contact</a>
+      </div>
+      <div className={styles.enquireNow}>
+        <button>Enquire now →</button>
+      </div>
+      <div className={styles.burger} onClick={toggleDropdown}>
+        ☰
       </div>
     </nav>
   );
